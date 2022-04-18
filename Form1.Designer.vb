caf6cc3d267b0663c14a47a8eef2bcbd7e55680f
TUF <TUF@LAPTOP-257ULQGP>
2022-04-18 23:29:28 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.txt2 = New System.Windows.Forms.TextBox()
+        Me.txt3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(191, 101)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Height(m)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(191, 158)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Weight(kg)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(191, 227)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "BMI"
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(334, 98)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(125, 27)
+        Me.txt1.TabIndex = 3
+        '
+        'txt2
+        '
+        Me.txt2.Location = New System.Drawing.Point(334, 158)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(125, 27)
+        Me.txt2.TabIndex = 4
+        '
+        'txt3
+        '
+        Me.txt3.BackColor = System.Drawing.Color.Yellow
+        Me.txt3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.txt3.Location = New System.Drawing.Point(334, 227)
+        Me.txt3.Name = "txt3"
+        Me.txt3.Size = New System.Drawing.Size(125, 27)
+        Me.txt3.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(279, 308)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 29)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txt3)
+        Me.Controls.Add(Me.txt2)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "BMI CALCULATOR"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents txt2 As TextBox
+    Friend WithEvents txt3 As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim str1 As Double = txt1.Text
+        Dim str2 As Double = txt2.Text
+        Dim Result As String
+
+        Dim str_bmi As String = str2 / (str1 * str1)
+
+        txt3.Text = str_bmi
+
+
+        If str_bmi < 18.5 Then
+            MessageBox.Show(" Underweight ")
+
+        ElseIf str_bmi <= 24.9 And str_bmi >= 18.5 Then
+            MessageBox.Show(" Normal weight ")
+
+        ElseIf str_bmi <= 29.9 And str_bmi >= 25 Then
+            MessageBox.Show(" Overweight ")
+
+        ElseIf str_bmi >= 30 Then
+            MessageBox.Show(" Obesity ")
+
+        Else
+            MessageBox.Show("Please insert number")
+
+        End If
+
+    End Sub
+End Class
